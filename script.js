43bcfed37a524e085ced7a029389d6143e275dc3
Mint <chawanluck_k@hotmail.com>
2022-08-05 23:15:11 +0700

    .

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ inputAdd.onkeyup = (event) => {
   if (event.key !== "Enter") return;
 
   //your code here
+  if (inputAdd.value === "") alert("Todo cannot be empty");
+  else {
+    addTodo(inputAdd.value, false);
+    inputAdd.value = "";
+    saveTodo();
+  }
 };
 
 function addTodo(title, completed) {
@@ -31,18 +37,65 @@ function addTodo(title, completed) {
   //your code here
   //append todo to HTML...
   //define buttons event...
+  const li = document.createElement("li");
+  // li.style = "none";
+  li.appendChild(span);
+  li.appendChild(doneBtn);
+  li.appendChild(deleteBtn);
+  li.appendChild(div);
+
+  todoCtn.prepend(li);
+
+  doneBtn.style.display = "none";
+  deleteBtn.style.display = "none";
+
+  let count = 0;
+  doneBtn.onclick = () => {
+    count++;
+    completed = count % 2;
+    span.style.textDecoration = completed ? "line-through" : "";
+    localStorage.setItem("line", (span.style.textDecoration = "line-through"));
+  };
+  deleteBtn.onclick = () => {
+    // localStorage.list = li;
+    const lst = JSON.stringify(li);
+    localStorage.setItem("list", lst);
+    todoCtn.removeChild(li);
+    localStorage.removeItem("list");
+  };
+  li.onmouseover = () => {
+    doneBtn.style.display = "";
+    deleteBtn.style.display = "";
+  };
+  li.onmouseout = () => {
+    doneBtn.style.display = "none";
+    deleteBtn.style.display = "none";
+  };
 }
 
 function saveTodo() {
   const data = [];
   for (const todoDiv of todoCtn.children) {
     //your code here
+    const todoObj = {};
+    todoObj.title = todoDiv.children[0].innerText;
+    todoObj.completed =
+      todoDiv.children[0].style.textDecoration === "line-through";
+    data.push(todoObj);
   }
   //your code here
+  const dataStr = JSON.stringify(data);
+  localStorage.setItem("todo", dataStr);
 }
 
 function loadTodo() {
   //your code here
+  const dataStr = localStorage.getItem("todo");
+  const data = JSON.parse(dataStr);
+
+  for (const todoObj of data) {
+    addTodo(todoObj.title, todoObj.completed);
+  }
 }
 
 loadTodo();
